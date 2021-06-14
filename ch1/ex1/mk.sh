8591d74ce49851306c60c96b8f5fc45e34108f73
Andrew Baker <andrew_j_baker2@hotmail.com>
2021-06-14 19:44:18 +0100

    Initial commit

---
+++ ch1/ex1/mk.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+g++ main.cpp
---
+++ ch1/ex1/valid.cpp
@@ -0,0 +1,13 @@
+int main() {
+  int foo;
+  auto foo2;
+  int bar = 10;
+  sum = 0;
+  float price = 5.3, cost = 10.1;
+  auto val = 5.6;
+  auto val = 5.6F;
+  auto var = val;
+  int a = 0, b = {1}, c(0);
+
+  return 0;
+}
